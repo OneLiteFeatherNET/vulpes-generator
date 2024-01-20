1eaa467298aeac236541c02319722565fc901f5a
theEvilReaper <steffenwx@gmail.com>
2024-01-20 21:12:12 +0100

    Add documentation

--- src/main/kotlin/net.theevilreaper.vulpes.generator/spec/GenerationSpec.kt
+++ src/main/kotlin/net.theevilreaper.vulpes.generator/spec/GenerationSpec.kt
@@ -1,3 +1,12 @@
 package net.theevilreaper.vulpes.generator.spec
 
+import org.jetbrains.annotations.ApiStatus
+
+/**
+ * The class is only a marker class which is required for the scanBasePackage process from Spring.
+ * @since 1.0.0
+ * @version 1.0.0
+ * @see org.springframework.context.annotation.ComponentScan and org.springframework.boot.autoconfigure.SpringBootApplication
+ */
+@ApiStatus.NonExtendable
 class GenerationSpec
