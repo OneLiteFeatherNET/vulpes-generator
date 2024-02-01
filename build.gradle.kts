b3fc59d31f8c9b486962cf208a897fd492add10d
theEvilReaper <steffenwx@gmail.com>
2024-02-01 11:37:51 +0100

    Upgrade to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ group = "net.theevilreaper"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
