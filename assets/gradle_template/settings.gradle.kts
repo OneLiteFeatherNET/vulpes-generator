41c8b0402339bfa8a27ebc0bdd3fd83f78dfdec7
theEvilReaper <steffenwx@gmail.com>
2024-02-01 11:50:11 +0100

    Update templates

--- assets/gradle_template/settings.gradle.kts
+++ assets/gradle_template/settings.gradle.kts
@@ -1,3 +1,11 @@
 rootProject.name = "vulpes"
 rootProject.buildFileName = "build.gradle.kts"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("jetbrains.annotation", "org.jetbrains", "annotations").version("24.1.0")
+            library("microtus", "net.onelitefeather.microtus", "Minestom").version("1.2.1")
+        }
+    }
+}
