75e3dafd7d94fc3e6c22607cd360dfafdfcfe510
theEvilReaper <steffenwx@gmail.com>
2024-03-26 15:31:48 +0100

    Improve template

--- assets/gradle_template/settings.gradle.kts
+++ assets/gradle_template/settings.gradle.kts
@@ -2,10 +2,14 @@ rootProject.name = "vulpes"
 rootProject.buildFileName = "build.gradle.kts"
 
 dependencyResolutionManagement {
+    repositories {
+        maven("https://jitpack.io")
+        mavenCentral()
+    }
     versionCatalogs {
         create("libs") {
             library("jetbrains.annotation", "org.jetbrains", "annotations").version("24.1.0")
-            library("microtus", "net.onelitefeather.microtus", "Minestom").version("1.2.1")
+            library("microtus", "net.onelitefeather.microtus", "Minestom").version("1.3.1")
         }
     }
 }
