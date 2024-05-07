aae5e08e01681939121ecd57622c2c26ddd85013
theEvilReaper <steffenwx@gmail.com>
2024-05-07 21:24:46 +0200

    Remove BlockGenerator integration

--- src/main/kotlin/net/theevilreaper/vulpes/generator/registry/RegistryProvider.kt
+++ src/main/kotlin/net/theevilreaper/vulpes/generator/registry/RegistryProvider.kt
@@ -1,17 +1,16 @@
 package net.theevilreaper.vulpes.generator.registry
 
 import net.theevilreaper.vulpes.generator.generation.java.AttributeGenerator
-import net.theevilreaper.vulpes.generator.generation.java.BlockGenerator
 import net.theevilreaper.vulpes.generator.generation.java.FontGenerator
+import net.theevilreaper.vulpes.generator.generation.java.ItemGenerator
 import net.theevilreaper.vulpes.generator.generation.java.NotificationGenerator
 import org.springframework.stereotype.Service
 
 @Service
 class RegistryProvider internal constructor(
     private val attributeGenerator: AttributeGenerator,
-    private val blockGenerator: BlockGenerator,
     private val fontGenerator: FontGenerator,
-    private val itemGenerator: BlockGenerator,
+    private val itemGenerator: ItemGenerator,
     private val notificationGenerator: NotificationGenerator
 ) {
 
@@ -23,12 +22,10 @@ class RegistryProvider internal constructor(
 
     private fun addJavaGenerators() {
         javaGeneratorRegistry.add(attributeGenerator)
-        javaGeneratorRegistry.add(blockGenerator)
         javaGeneratorRegistry.add(fontGenerator)
         javaGeneratorRegistry.add(itemGenerator)
         javaGeneratorRegistry.add(notificationGenerator)
     }
-
 
     fun getRegistry(): GeneratorRegistry {
         return javaGeneratorRegistry
