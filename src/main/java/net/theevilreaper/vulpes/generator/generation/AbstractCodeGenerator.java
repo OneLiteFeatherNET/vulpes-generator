7ac6acfb9696c55a5e9d269059644cff78a03552
theEvilReaper <steffenwx@gmail.com>
2025-02-23 17:07:14 +0100

    Add documentation and update variable definition

--- src/main/java/net/theevilreaper/vulpes/generator/generation/AbstractCodeGenerator.java
+++ src/main/java/net/theevilreaper/vulpes/generator/generation/AbstractCodeGenerator.java
@@ -14,7 +14,11 @@ import java.util.List;
 
 public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Generator {
 
+    private static final String EMPTY_FILES_TO_WRITE = "No files to write. Skipping the file write";
+
     protected static final String BASE_PACKAGE = "net.reaper.vulpes";
+    protected static final String EMPTY_COMPONENT = "empty()";
+
     protected Logger logger = LoggerFactory.getLogger(AbstractCodeGenerator.class);
 
     protected final String className;
@@ -24,7 +28,6 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
 
     protected List<JavaFile> filesToGenerate = new ArrayList<>();
     protected TypeSpec.Builder classBuilder = getClassType(classType);
-    protected final String emptyComponent = "empty()";
 
     protected AbstractCodeGenerator(@NotNull String className, @NotNull String packageName, TypeSpec.Kind classType) {
         this.className = className;
@@ -36,23 +39,40 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
         this(className, packageName, TypeSpec.Kind.CLASS);
     }
 
-    protected void writeFiles(List<JavaFile> fileList, @NotNull Path outputFolder) {
+    /**
+     * Writes a list of files to the given output folder.
+     *
+     * @param fileList     list of files to write
+     * @param outputFolder the output folder
+     */
+    protected void writeFiles(@NotNull List<JavaFile> fileList, @NotNull Path outputFolder) {
         if (fileList.isEmpty()) {
-            logger.info("No files to write. Skipping the file write");
+            logger.info(EMPTY_FILES_TO_WRITE);
             return;
         }
         write(fileList, outputFolder);
     }
 
+    /**
+     * Writes the files to the given output folder.
+     *
+     * @param outputFolder the output folder
+     */
     protected void writeFiles(@NotNull Path outputFolder) {
         if (filesToGenerate.isEmpty()) {
-            logger.info("No files to write. Skipping the file write");
+            logger.info(EMPTY_FILES_TO_WRITE);
             return;
         }
         write(filesToGenerate, outputFolder);
     }
 
-    private void write(List<JavaFile> files, @NotNull Path outputFolder) {
+    /**
+     * Writes the given files to the output folder.
+     *
+     * @param files        the files to write
+     * @param outputFolder the output folder
+     */
+    private void write(@NotNull List<JavaFile> files, @NotNull Path outputFolder) {
         files.forEach(file -> {
             try {
                 file.writeTo(outputFolder);
@@ -74,7 +94,7 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
 
     protected abstract List<T> getModels();
 
-    private TypeSpec.Builder getClassType(TypeSpec.Kind classType) {
+    private @NotNull TypeSpec.Builder getClassType(@NotNull TypeSpec.Kind classType) {
         return switch (classType) {
             case CLASS -> TypeSpec.classBuilder(className);
             case INTERFACE -> TypeSpec.interfaceBuilder(className);
