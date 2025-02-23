e68e6b2fa18b2361cfe7ac857d1733be25a84a6e
theEvilReaper <steffenwx@gmail.com>
2025-02-23 17:08:51 +0100

    Overhaul generator registry

--- src/main/java/net/theevilreaper/vulpes/generator/registry/RegistryProvider.java
+++ src/main/java/net/theevilreaper/vulpes/generator/registry/RegistryProvider.java
@@ -8,26 +8,46 @@ import net.theevilreaper.vulpes.generator.generation.java.ItemGenerator;
 import net.theevilreaper.vulpes.generator.generation.java.NotificationGenerator;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Provides a pre-configured {@link GeneratorRegistry} instance
+ * with all available generators registered.
+ */
 @Singleton
 public final class RegistryProvider {
 
     private final GeneratorRegistry generatorRegistry;
 
+    /**
+     * Creates a new instance of the {@link RegistryProvider}.
+     * <p>
+     * Automatically registers all provided generators into the {@link GeneratorRegistry}.
+     * </p>
+     *
+     * @param attributeGenerator    The attribute generator.
+     * @param fontGenerator         The font generator.
+     * @param itemGenerator         The item generator.
+     * @param notificationGenerator The notification generator.
+     */
     @Inject
-    public RegistryProvider(
+    RegistryProvider(
             @NotNull AttributeGenerator attributeGenerator,
             @NotNull FontGenerator fontGenerator,
             @NotNull ItemGenerator itemGenerator,
             @NotNull NotificationGenerator notificationGenerator
     ) {
-        this.generatorRegistry = new GeneratorRegistry();
+        this.generatorRegistry = new VulpesGeneratorRegistry();
         this.generatorRegistry.add(attributeGenerator);
         this.generatorRegistry.add(fontGenerator);
         this.generatorRegistry.add(itemGenerator);
         this.generatorRegistry.add(notificationGenerator);
     }
 
-    public GeneratorRegistry getGeneratorRegistry() {
+    /**
+     * Returns the generator registry containing all registered generators.
+     *
+     * @return The {@link GeneratorRegistry} instance.
+     */
+    public @NotNull GeneratorRegistry getGeneratorRegistry() {
         return generatorRegistry;
     }
 }
