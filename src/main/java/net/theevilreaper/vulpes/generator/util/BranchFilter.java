7dc387429ddcc0b0119ef7778450ad2e70f2230f
theEvilReaper <steffenwx@gmail.com>
2025-02-23 17:04:45 +0100

    Add missing annotation

--- src/main/java/net/theevilreaper/vulpes/generator/util/BranchFilter.java
+++ src/main/java/net/theevilreaper/vulpes/generator/util/BranchFilter.java
@@ -11,11 +11,12 @@ public final class BranchFilter {
 
     /**
      * Filters the branches and returns a list with the filtered branches.
-     * @param branches The branches which should be filtered
+     *
+     * @param branches  The branches which should be filtered
      * @param predicate The predicate which should be used for the filtering
      * @return a list with the filtered branches
      */
-    public static @NotNull List<String> filterBranches(@NotNull List<String> branches, Predicate<String> predicate) {
+    public static @NotNull List<String> filterBranches(@NotNull List<String> branches, @NotNull Predicate<String> predicate) {
         return branches.stream().filter(predicate).toList();
     }
 
