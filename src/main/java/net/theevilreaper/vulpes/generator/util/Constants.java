69b7e1bf9d0b2acb578e1db1536e9cce008dd79b
theEvilReaper <steffenwx@gmail.com>
2025-02-23 19:16:18 +0100

    Remove unused constants

--- src/main/java/net/theevilreaper/vulpes/generator/util/Constants.java
+++ src/main/java/net/theevilreaper/vulpes/generator/util/Constants.java
@@ -12,18 +12,14 @@ public final class Constants {
 
     public static final String TEMP_PREFIX = "vulpes-";
     public static final String ZIP_FILE_NAME = "gradle_template.zip";
-    public static final String VERSION = "1.0.0-TEST";
 
     public static final String GITLAB_CI_YML = ".gitlab-ci.yml";
-
-    public static final String META_DATA_VARIABLE = "data";
 
     public static final String GRADLE_PROPERTIES = "gradle.properties";
     public static final String OUT_PUT_FOLDER = "out";
     public static final String JAVA_MAIM_FOLDER = "src/main/java";
 
     public static final String EMPTY_STRING = "";
-    public static final String SPACE_STRING = " ";
 
     private Constants() {
         throw new UnsupportedOperationException("This class cannot be instantiated");
