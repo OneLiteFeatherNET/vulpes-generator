646e2f83009f0ec2a1a616e00020555af58b9a4d
theEvilReaper <steffenwx@gmail.com>
2025-04-15 13:37:41 +0200

    Improve variable type usage

--- src/main/java/net/theevilreaper/vulpes/generator/controller/BuildInformationHandler.java
+++ src/main/java/net/theevilreaper/vulpes/generator/controller/BuildInformationHandler.java
@@ -7,11 +7,14 @@ import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
 import net.theevilreaper.vulpes.generator.object.BuildInformation;
 import net.theevilreaper.vulpes.generator.properties.GitlabProperties;
-import org.gitlab4j.api.Constants;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.Pager;
+import org.gitlab4j.api.ProjectApi;
+import org.gitlab4j.api.models.Package;
 import org.gitlab4j.api.models.PackageFilter;
 import org.gitlab4j.api.models.Project;
+import org.gitlab4j.models.Constants;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -22,28 +25,28 @@ import java.util.Map;
 public class BuildInformationHandler {
 
     private final GitLabApi gitLabApi;
+    private final PackageFilter standardFilter;
 
     @Inject
     public BuildInformationHandler(@NotNull GitlabProperties gitlabProperties) {
         this.gitLabApi = new GitLabApi(gitlabProperties.gitlabUrl(), gitlabProperties.accessToken());
+        standardFilter = new PackageFilter()
+                .withOrderBy(Constants.PackageOrderBy.CREATED_AT)
+                .withSortOder(Constants.SortOrder.DESC);
     }
 
     @Get(value = "/data", produces = MediaType.APPLICATION_JSON)
     public @NotNull HttpResponse<BuildInformation> getBuildInformation() throws GitLabApiException {
-        var projectApi = gitLabApi.getProjectApi();
+        ProjectApi projectApi = gitLabApi.getProjectApi();
         List<Project> projects = projectApi.getProjects("aves");
 
-
-        var avesProject = projects.getFirst();
-        var filter = new PackageFilter().withOrderBy(Constants.PackageOrderBy.CREATED_AT)
-                .withSortOder(Constants.SortOrder.DESC);
-
-        var latestReleases = gitLabApi.getPackagesApi().getPackages(avesProject.getId(), filter, 1);
+        Project avesProject = projects.getFirst();
+        Pager<Package> latestReleases = gitLabApi.getPackagesApi().getPackages(avesProject.getId(), standardFilter, 1);
 
         if (!latestReleases.hasNext()) {
             return HttpResponse.ok(new BuildInformation(Map.of("error", "No releases found")));
         }
-        var release = latestReleases.first().getFirst();
+        Package release = latestReleases.first().getFirst();
         Map<String, String> data = new HashMap<>();
         data.put("version", release.getVersion());
         data.put("created", release.getCreatedAt().toString());
