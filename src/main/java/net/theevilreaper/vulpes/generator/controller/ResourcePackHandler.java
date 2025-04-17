1427b6b2245b65024002b7773f90605b7919ae9c
theEvilReaper <steffenwx@gmail.com>
2025-04-17 08:50:09 +0200

    Add new controller class

--- src/main/java/net/theevilreaper/vulpes/generator/controller/ResourcePackHandler.java
+++ src/main/java/net/theevilreaper/vulpes/generator/controller/ResourcePackHandler.java
@@ -4,12 +4,25 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import jakarta.inject.Inject;
+import net.theevilreaper.vulpes.generator.domain.pack.PackRequest;
+import net.theevilreaper.vulpes.generator.domain.pack.PackResultDTO;
+import net.theevilreaper.vulpes.generator.generation.ResourcePackService;
 
 @Controller("/resourcepack")
 public class ResourcePackHandler {
 
+    private final ResourcePackService resourcePackService;
+
+    @Inject
+    public ResourcePackHandler(ResourcePackService resourcePackService) {
+        this.resourcePackService = resourcePackService;
+    }
+
     @Get(value = "/", produces = "application/json")
-    public HttpResponse<Object> getResourcePack() {
+    public HttpResponse<PackResultDTO> getResourcePack(
+            @Body PackRequest request
+    ) {
         return HttpResponse.ok();
     }
 }
