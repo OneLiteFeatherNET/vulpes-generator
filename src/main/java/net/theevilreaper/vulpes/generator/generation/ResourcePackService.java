40c1d72e8fa335a6949089cb7a4c81136d751656
theEvilReaper <steffenwx@gmail.com>
2025-04-17 08:49:39 +0200

    Add ResourcePackService class

---
+++ src/main/java/net/theevilreaper/vulpes/generator/generation/ResourcePackService.java
@@ -0,0 +1,13 @@
+package net.theevilreaper.vulpes.generator.generation;
+
+import jakarta.inject.Singleton;
+import net.theevilreaper.vulpes.generator.domain.pack.PackOption;
+import org.jetbrains.annotations.NotNull;
+
+@Singleton
+public class ResourcePackService {
+
+    public void generateResourcePack(@NotNull PackOption packOption) {
+
+    }
+}
