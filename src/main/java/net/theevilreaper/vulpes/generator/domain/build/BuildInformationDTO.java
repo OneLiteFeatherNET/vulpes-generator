76deb70ec12c9983201baa09cedeef76fb488ca4
theEvilReaper <steffenwx@gmail.com>
2025-04-17 07:52:01 +0200

    Move BuildInformation class and rename it

---
+++ src/main/java/net/theevilreaper/vulpes/generator/domain/build/BuildInformationDTO.java
@@ -0,0 +1,18 @@
+package net.theevilreaper.vulpes.generator.domain.build;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+@Schema(
+        requiredProperties = {
+                "data"
+        }
+)
+@Introspected
+@Serdeable
+public record BuildInformationDTO(@NotNull Map<String, String> data) {
+}
