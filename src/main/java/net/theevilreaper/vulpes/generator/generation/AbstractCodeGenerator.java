599d276ecfce98366f4f3025604b96f3c5d8f809
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-04 14:57:33 +0200

    More code documentation

--- src/main/java/net/theevilreaper/vulpes/generator/generation/AbstractCodeGenerator.java
+++ src/main/java/net/theevilreaper/vulpes/generator/generation/AbstractCodeGenerator.java
@@ -12,6 +12,16 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The {@link AbstractCodeGenerator} is an abstract implementation of the {@link Generator} interface that provides common functionality for generating code files.
+ * This class is designed to generate specific code files for the vulpes environment.
+ * If your use case doesn't fit into this case, you should implement the {@link Generator} interface directly.
+ *
+ * @param <T> the type of the model that this generator will work with
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Generator {
 
     private static final String EMPTY_FILES_TO_WRITE = "No files to write. Skipping the file write";
@@ -23,12 +33,18 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
 
     protected final String className;
     protected final String packageName;
-
-    private TypeSpec.Kind classType;
+    protected final TypeSpec.Kind classType;
 
     protected List<JavaFile> filesToGenerate = new ArrayList<>();
     protected TypeSpec.Builder classBuilder;
 
+    /**
+     * Creates a new instance of the code generator with the specified class name, package name, and class type.
+     *
+     * @param className   the name of the class to generate
+     * @param packageName the package name for the generated class
+     * @param classType   the type of the class to generate (e.g., CLASS, INTERFACE, ENUM, ANNOTATION)
+     */
     protected AbstractCodeGenerator(@NotNull String className, @NotNull String packageName, TypeSpec.Kind classType) {
         this.className = className;
         this.packageName = packageName;
@@ -36,6 +52,12 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
         this.classBuilder = getClassType(this.classType);
     }
 
+    /**
+     * Creates a new instance of the code generator with the specified class name and package name.
+     *
+     * @param className   the name of the class to generate
+     * @param packageName the package name for the generated class
+     */
     protected AbstractCodeGenerator(@NotNull String className, @NotNull String packageName) {
         this(className, packageName, TypeSpec.Kind.CLASS);
     }
@@ -83,18 +105,38 @@ public abstract class AbstractCodeGenerator<T extends VulpesModel> implements Ge
         });
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void cleanup() {
         this.filesToGenerate.clear();
         this.classBuilder = getClassType(this.classType);
     }
 
+    /**
+     * Changes the format of the given name from UPPER_CAMEL to UPPER_UNDERSCORE.
+     *
+     * @param name the name to change format
+     * @return the name in UPPER_UNDERSCORE format
+     */
     protected String changeFormat(String name) {
         return CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, name);
     }
 
+    /**
+     * Returns the models that this generator will work with.
+     *
+     * @return a list of models
+     */
     protected abstract List<T> getModels();
 
+    /**
+     * Returns the right TypeSpec.Builder based on the class type.
+     *
+     * @param classType the class type to get the TypeSpec.Builder for
+     * @return the TypeSpec.Builder for the given class type
+     */
     private @NotNull TypeSpec.Builder getClassType(@NotNull TypeSpec.Kind classType) {
         return switch (classType) {
             case CLASS -> TypeSpec.classBuilder(className);
--- src/main/java/net/theevilreaper/vulpes/generator/generation/Generator.java
+++ src/main/java/net/theevilreaper/vulpes/generator/generation/Generator.java
@@ -4,6 +4,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 
+/**
+ * The {@link Generator} interface defines the base method structure which each implementations needs to work with the current system approach.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public interface Generator {
 
     /**
