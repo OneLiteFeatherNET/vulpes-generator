a5d09e53bd0253b5fe7fe34ee61780512023f051
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-04 14:07:50 +0200

    Overhaul generator registry

--- src/main/java/net/theevilreaper/vulpes/generator/registry/VulpesGeneratorRegistry.java
+++ src/main/java/net/theevilreaper/vulpes/generator/registry/VulpesGeneratorRegistry.java
@@ -1,56 +1,83 @@
 package net.theevilreaper.vulpes.generator.registry;
 
+import jakarta.inject.Singleton;
 import net.onelitefeather.vulpes.api.model.VulpesModel;
 import net.theevilreaper.vulpes.generator.generation.AbstractCodeGenerator;
+import net.theevilreaper.vulpes.generator.generation.Generator;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnmodifiableView;
 
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static org.jetbrains.annotations.ApiStatus.*;
+
 /**
- * Implementation of {@link GeneratorRegistry} using an in-memory {@link ConcurrentHashMap}.
+ * The {@link VulpesGeneratorRegistry} is the default registry implementation of the {@link GeneratorRegistry}
+ * to manage different implementation of a {@link AbstractCodeGenerator}.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
  */
-@ApiStatus.Internal
-@ApiStatus.NonExtendable
+@Internal
+@NonExtendable
+@Singleton
 final class VulpesGeneratorRegistry implements GeneratorRegistry {
 
-    private final Map<String, AbstractCodeGenerator<? extends VulpesModel>> generators;
+    private final Map<Class<? extends Generator>, Generator> generators;
 
     /**
-     * Creates an empty generator registry.
+     * Creates a new instance of the registry.
      */
-    VulpesGeneratorRegistry() {
+    private VulpesGeneratorRegistry() {
         this.generators = new ConcurrentHashMap<>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public AbstractCodeGenerator<? extends VulpesModel> add(@NotNull AbstractCodeGenerator<? extends VulpesModel> generator) {
-        return this.generators.put(generator.getName(), generator);
+    public void add(@NotNull Generator generator) {
+        generators.put(generator.getClass(), generator);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public AbstractCodeGenerator<? extends VulpesModel> remove(@NotNull String name) {
-        return this.generators.remove(name);
+    public @Nullable <T extends Generator> Generator remove(@NotNull Class<T> generatorClass) {
+        return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public AbstractCodeGenerator<? extends VulpesModel> get(@NotNull String name) {
-        return this.generators.get(name);
+    public @Nullable <T extends Generator> Generator get(@NotNull Class<T> generatorClass) {
+        return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void triggerAll(@NotNull Path path) {
         this.generators.values().forEach(generator -> generator.generate(path));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Contract(pure = true)
-    public @NotNull @UnmodifiableView Map<String, AbstractCodeGenerator<? extends VulpesModel>> getAll() {
+    public @NotNull @UnmodifiableView Map<Class<? extends Generator>, Generator> getAll() {
         return Collections.unmodifiableMap(generators);
     }
 }
