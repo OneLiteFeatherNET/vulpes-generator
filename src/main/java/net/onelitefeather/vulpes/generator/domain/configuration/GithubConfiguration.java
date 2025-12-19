c53be9293295fe25fec8dd0cf0b80b7e6e47e9ed
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-12-19 21:58:37 +0100

    chore(configuration): add new values to the Github configuration

--- src/main/java/net/onelitefeather/vulpes/generator/domain/configuration/GithubConfiguration.java
+++ src/main/java/net/onelitefeather/vulpes/generator/domain/configuration/GithubConfiguration.java
@@ -9,13 +9,19 @@ import net.onelitefeather.vulpes.generator.domain.client.GithubClient;
  *
  * @param owner the owner / organization of the repository
  * @param repo  the repository name
+ * @param username the username for authentication
+ * @param password the password or token for authentication
+ * @param cloneUrl the clone URL of the repository
  * @author theEvilReaper
- * @version 1.0.0
+ * @version 1.1.0
  * @since 1.0.0
  */
 @ConfigurationProperties("github")
 public record GithubConfiguration(
         String owner,
-        String repo
+        String repo,
+        String username,
+        String password,
+        String cloneUrl
 ) {
 }
