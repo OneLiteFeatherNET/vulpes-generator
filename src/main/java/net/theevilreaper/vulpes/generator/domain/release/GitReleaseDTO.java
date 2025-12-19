47497efd7b73a0403eb268500009bd405ddc1c9d
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-12-19 09:01:06 +0100

    chore(dto): add swagger annotations

--- src/main/java/net/theevilreaper/vulpes/generator/domain/release/GitReleaseDTO.java
+++ src/main/java/net/theevilreaper/vulpes/generator/domain/release/GitReleaseDTO.java
@@ -1,6 +1,7 @@
 package net.theevilreaper.vulpes.generator.domain.release;
 
 import io.micronaut.serde.annotation.Serdeable;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.OffsetDateTime;
 
@@ -16,8 +17,11 @@ import java.time.OffsetDateTime;
  */
 @Serdeable
 public record GitReleaseDTO(
+        @Schema(description = "The version of the release", example = "1.0.0", requiredMode = Schema.RequiredMode.REQUIRED)
         String version,
+        @Schema(description = "The url to the release on Github", example = "https://github.com/OneLiteFeatherNET/Vulpes/releases/tag/1.0.0")
         String url,
+        @Schema(description = "The date when the release was published", example = "2021-05-20T12:00:00Z", requiredMode = Schema.RequiredMode.REQUIRED)
         OffsetDateTime publishedAt
 ) {
 
