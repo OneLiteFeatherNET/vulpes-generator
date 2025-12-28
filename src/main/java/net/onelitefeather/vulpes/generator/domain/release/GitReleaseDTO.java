209d9b45b6d20597880d0942ab4162f7c000a357
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2025-12-28 11:25:55 +0100

    feature: enhance GitRelease dto model (#79)
    * chore(git): add new values to the git release object
    
    * chore(test): update constructor usage
    
    * chore(model): add comment for variable naming
    
    ---------
    
    Co-authored-by: theEvilReaper <theEvilReaper@users.noreply.github.com>

--- src/main/java/net/onelitefeather/vulpes/generator/domain/release/GitReleaseDTO.java
+++ src/main/java/net/onelitefeather/vulpes/generator/domain/release/GitReleaseDTO.java
@@ -8,9 +8,11 @@ import java.time.OffsetDateTime;
 /**
  * The {@link GitReleaseDTO} is a data transfer object which is sent via API to inform clients about the new release
  *
- * @param version     the version of the release
- * @param url         the url to the release on Github
- * @param publishedAt the date when the release was published
+ * @param version      the version of the release
+ * @param url          the url to the release on Github
+ * @param publishedAt  the date when the release was published
+ * @param prerelease   whether the release is a prerelease or not
+ * @param targetCommit targetCommit the commit hash of the release or branch
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
@@ -22,7 +24,12 @@ public record GitReleaseDTO(
         @Schema(description = "The url to the release on Github", example = "https://github.com/OneLiteFeatherNET/Vulpes/releases/tag/1.0.0")
         String url,
         @Schema(description = "The date when the release was published", example = "2021-05-20T12:00:00Z", requiredMode = Schema.RequiredMode.REQUIRED)
-        OffsetDateTime publishedAt
+        OffsetDateTime publishedAt,
+        @Schema(description = "Whether the release is a prerelease or not", example = "false", requiredMode = Schema.RequiredMode.REQUIRED)
+        boolean prerelease,
+        @Schema(description = "The commit hash of the release or branch", example = "master", requiredMode = Schema.RequiredMode.REQUIRED)
+        // The naming of this comes from the official api https://docs.github.com/en/rest/releases/releases. Search for target_commitish
+        String targetCommitish
 ) {
 
     /**
@@ -35,7 +42,9 @@ public record GitReleaseDTO(
         return new GitReleaseDTO(
                 release.tagName(),
                 release.htmlUrl(),
-                release.publishedAt()
+                release.publishedAt(),
+                release.prerelease(),
+                release.targetCommitish()
         );
     }
 
@@ -46,7 +55,7 @@ public record GitReleaseDTO(
      * @return the created {@link GitReleaseDTO} instance
      */
     public static GitReleaseDTO fromTag(String tag) {
-        return new GitReleaseDTO(tag, null, null);
+        return new GitReleaseDTO(tag, null, null, false, "");
     }
 
     /**
@@ -55,6 +64,6 @@ public record GitReleaseDTO(
      * @return the created {@link GitReleaseDTO} instance
      */
     public static GitReleaseDTO unknown() {
-        return new GitReleaseDTO("unknown", null, null);
+        return new GitReleaseDTO("unknown", null, null, false, "");
     }
 }
